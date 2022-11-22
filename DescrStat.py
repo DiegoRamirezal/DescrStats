8afd119d81a3175a7f9fecb8d294cea4f2a82a5f
DiegoRamirezal <100793811+DiegoRamirezal@users.noreply.github.com>
2022-11-22 16:24:07 -0300

    Update DescrStat.py

--- DescrStat.py
+++ DescrStat.py
@@ -1,11 +1,19 @@
-'''
-------------------------------------------------------------------------
-This script defines a function DescrStat() that gives descriptive
-statistics for a comma delimited text file of one variable in which the
-data are in one column and each observation is a row.
-------------------------------------------------------------------------
-'''
-
+ '''
+    --------------------------------------------------------------------
+    Esta función imprime y retorna estadística descriptiva de un archivo de texto delimitado por comas, de una sola variable
+    --------------------------------------------------------------------
+    INPUTS:
+    datafile = string, path del archivo de datos a usar en la función.
+    OTRAS FUNCIONES Y ARCHIVOS LLAMADOS POR ESTA FUNCIÓN: None
+    OBJETOS CREADOS EN LA FUNCIÓN:
+    data = (N,) vector, data de datafile
+    data_mean = scalar, promedio de los datos
+    data_std = scalar >= 0, desviación estándar de la data
+    data_var = scalar >= 0, varianza de la data
+    ARCHIVOS CREADOS PRO ESTA FUNCIÓN: None
+    RETURNS: data_mean, data_std, data_var
+    --------------------------------------------------------------------
+    '''
 # Import packages
 import numpy as np
 
